493bb8c2761c8dc3bd11687ca55066528813abee
Serge Ts <sergets@ya.ru>
2016-03-09 17:39:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,17 @@ app.get('/', function(request, response) {
     response.json({ ok : true });
 });
 
-app.get('/:type/:route', function(request, response) {
-    timetable.getAllTimetables(request.params.type, request.params.route)
-        .then(response.json.bind(response))
-        .catch(response.json.bind(response));
-});
+app
+    .get('/:type/:route', function(request, response) {
+        timetable.getAllTimetables(request.params.type, request.params.route)
+            .then(response.json.bind(response))
+            .catch(response.json.bind(response));
+    })
+    .get('/:type/:route/compact', function(request, response) {
+        timetable.getAllTimetables(request.params.type, request.params.route, true)
+            .then(response.json.bind(response))
+            .catch(response.json.bind(response));
+    });
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
