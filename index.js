54663cf93ae6947a0bf0be51ba41461f5ed601fd
Serge Ts <sergets@ya.ru>
2016-04-10 16:47:01 +0300

    sopa header

--- index.js
+++ index.js
@@ -77,6 +77,7 @@ app.get('/', function(request, response) {
 
 app
     .get('/:type', function(request, response) {
+        response.header('Access-Control-Allow-Origin', '*');
         timetable.getAllRoutes(request.params.type)
             .then(response.json.bind(response))
             .catch(response.json.bind(response));
@@ -84,31 +85,26 @@ app
     .get('/:type/:route', function(request, response) {
         var file = request.params.type + '/' + request.params.route + '.json';
         
+        response.header('Access-Control-Allow-Origin', '*');
         yadisk.getData(file)
-            .then(function(data) { return new Date() - new Date(data.modified) < 1000 * 86400 * 7; })
-            .then(function(isNewEnough) {
-                return isNewEnough?
+            .then(function(data) {
+                return data.name?
                     yadisk.read(file).then(compactifier.decompactifyTimetables.bind(compactifier)) :
                     fetchTimetableFromServer(request.params.type, request.params.route);
-            })      
-            .then(response.json.bind(response))
-            .catch(response.json.bind(response));
-    })
-    .get('/:type/:route/original', function(request, response) {
-        fetchTimetableFromServer(request.params.type, request.params.route)  
+            })    
             .then(response.json.bind(response))
             .catch(response.json.bind(response));
     })
     .get('/:type/:route/compact', function(request, response) {
         var file = request.params.type + '/' + request.params.route + '.json';
         
+        response.header('Access-Control-Allow-Origin', '*');
         yadisk.getData(file)
-            .then(function(data) { return new Date() - new Date(data.modified) < 1000 * 86400 * 7; })
-            .then(function(isNewEnough) {
-                return isNewEnough?
+            .then(function(data) { 
+                return data.name?
                     yadisk.read(file) :
                     fetchTimetableFromServer(request.params.type, request.params.route).then(compactifier.compactifyTimetables.bind(compactifier));
-            })      
+            })
             .then(response.json.bind(response))
             .catch(response.json.bind(response));
     });
